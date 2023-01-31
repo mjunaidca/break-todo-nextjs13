5546885c1920f38d96318ebde2be222a468a7247
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-02-01 04:04:31 +0500

    Finalized ToDo App

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import React from 'react'
-import { HStack, VStack, Text, IconButton, Divider, Spacer, Badge } from '@chakra-ui/react'
-import { MinusIcon } from '@chakra-ui/icons'
+import { HStack, VStack, Text, IconButton, Divider, Spacer, Badge, Checkbox  } from '@chakra-ui/react'
+import { CloseIcon, BellIcon } from '@chakra-ui/icons'
 
 
 export default function TodoList({todos, deleteTodo}: any) {
+ 
 
   if(!todos.length) {
     return (
@@ -20,20 +21,22 @@ export default function TodoList({todos, deleteTodo}: any) {
       borderWidth='3px'
       p='6'
       borderRadius='2xl'
-      width='75%'
-      maxW={{base: '80vw', sm:'50vw', lg: '50vw', xl:'50vw'}}
+      width='70%'
+      maxW={{base: '90vw', sm:'50vw', lg: '50vw', xl:'50vw'}}
       alignItems='stretch'
       >
       
       {todos.map((todo:any) => (
         <HStack key={todo.id}>
-          <Text fontWeight={'bold'}>{todo.body}</Text>
+          <Checkbox type={'checkbox'} colorScheme='red'  size='3xl' spacing='2rem' icon={<BellIcon />}>
+          <Text fontWeight={'bold'}>{todo.body}</Text></Checkbox>
           <Spacer />
-          <IconButton bg='red.400' aria-label='trash' icon={<MinusIcon />} onClick={()=>deleteTodo(todo.id)}/>
-          
+         
+          <IconButton bg='red.500' aria-label='trash' icon={<CloseIcon />} onClick={()=>deleteTodo(todo.id)}/>
         </HStack>
       ))}
 
     </VStack>
   )
 }
+
